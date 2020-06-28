3a02215b33294bb70fa6a38e3d96ff73904c258e
fkaus5555 <65529185+fkaus5555@users.noreply.github.com>
2020-06-28 23:15:42 +0900

    Update settings.py

--- dj_dashboard/settings.py
+++ dj_dashboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*j9r)woop=j42k_n)t3#r+$745b*y_rsgtj&a+$ott1k73q9*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
